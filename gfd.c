8425d80ec147efdf55cac4ab140ab5200176c0e8
venkatroshan <35727735+venkatroshan@users.noreply.github.com>
2018-02-08 16:26:58 +0530

    Create gfd.c

---
+++ gfd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int [a]=15;
+int n,k,i;
+printf("\n enter the value of n");
+scanf("%d ",&n);
+printf("\n enter the vaue of k");
+scanf("%d",&k);
+if(i=1,i<n,n++)
+{
+printf("\n enter the value");
+scanf("%d",&a);
+if(i=1,i<k,k++);
+scanf("%d"&a);
+sum=a+sum [a]
+
+}
+}
