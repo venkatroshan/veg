f6f84585704f259697cb3be0e98eb0b3e2c3d468
venkatroshan <35727735+venkatroshan@users.noreply.github.com>
2018-02-08 16:40:48 +0530

    Create df .c

---
+++ df .c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+clrscr();
+printf("enter the number");
+scanf("%d",&i);
+if(i>0)
+{
+  printf("the given number is positive");
+  }
+  else if(i<0)
+  {
+    printf("the given number is negative");
+    }
+    else
+    {
+    printf("the given number is zero");
+    }
+   getch();
+   }
+© 2018 GitHub, Inc.
